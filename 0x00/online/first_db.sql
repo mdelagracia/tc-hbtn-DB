e7786f3e6f9c64ba030681f660f0097c2fb38912
mdelagracia <mdelagracia@ciandt.com>
2022-07-21 17:48:54 -0300

    Task 4

---
+++ 0x00/online/first_db.sql
@@ -0,0 +1,9 @@
+insert into CLIENTE values (7,'Till','till@techcamps.com',94854343);
+insert into CLIENTE values (8,'Lindemann','lindemann@techcamps.com',4342342);
+insert into CLIENTE values (9,'Pedrinho','pedrinho@techcamps.com',954354376348);
+insert into CLIENTE values (10,'DonaDirce','dirce@techcamps.com',943434348);
+insert into CLIENTE values (11,'Lourdes','croche@techcamps.com',9454576348);
+insert into CLIENTE values (12,'Lucimara','luci@techcamps.com',944342428);
+select * from CLIENTE;
+
+
