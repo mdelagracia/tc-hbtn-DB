973848ea3eaf7345a35bf141839349d25388dbfd
mdelagracia <mdelagracia@ciandt.com>
2022-07-25 22:42:23 -0300

    Task 0

--- 0x01/java_jdbc/ConnectionSQLite.java
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -1,19 +1,20 @@
 import java.sql.*;
 
 public class ConnectionSQLite {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-       try {
-           Class.forName("org.sqlite.JDBC");
-           Connection conexao = DriverManager.getConnection(
-                   "jdbc:sqlite:sqlite_database_2022.db");
+    public static void main(String[] args) {
+        initConnection();
+    }
 
-           System.out.println("Conectado!");
-           conexao.close();
-       } catch (ClassNotFoundException e) {
-           throw new RuntimeException(e);
-       } catch (SQLException e) {
-           throw new RuntimeException(e);
-       }
-
+    public static void initConnection() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conexao = DriverManager.getConnection(
+                    "jdbc:sqlite:sqlite_database_2022.db");
+            conexao.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -1,28 +1,18 @@
 import java.sql.*;
 import java.util.Enumeration;
 
-import static oracle.jdbc.OracleDriver.getDriverVersion;
-
 public class PrintJDBCDrivers {
     public static void main(String args[]) {
         try {
-
             Enumeration<Driver> e = DriverManager.getDrivers();
 
-            while(e.hasMoreElements()){
+            while (e.hasMoreElements()) {
                 Driver driver = e.nextElement();
                 System.out.println(driver.getMajorVersion());
                 System.out.println(driver.getClass());
             }
-
-
-
-
-
         } catch (Exception e) {
             System.out.println(e);
         }
-
     }
 }
-
