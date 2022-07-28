9db0f2625081db8e99be98ef2e1a26fe427e1eab
mdelagracia <mdelagracia@ciandt.com>
2022-07-27 22:35:46 -0300

    Task 2

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE Cliente (ID INTEGER PRIMARY KEY autoincrement, Nome           CHAR(50)    NOT NULL,  Idade          INTEGER    NOT NULL,  cpf            CHAR(14)   NOT NULL,  rg             CHAR(14)   NOT NULL);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
